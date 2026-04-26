1d31024c1740fe916b3c3a0f443bfceb814572c5
okech martin <odhismartin0@gmail.com>
2026-04-26 19:50:20 +0100

    update all the file

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your code here!
+async function fetchPosts() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await response.json();
+  displayPosts(posts);
+}
+
+function displayPosts(posts) {
+  const postList = document.querySelector("#post-list");
+  posts.forEach((post) => {
+    const li = document.createElement("li");
+    const h1 = document.createElement("h1");
+    const p = document.createElement("p");
+
+    h1.textContent = post.title;
+    p.textContent = post.body;
+
+    li.appendChild(h1);
+    li.appendChild(p);
+    postList.appendChild(li);
+  });
+}
+
+fetchPosts();
